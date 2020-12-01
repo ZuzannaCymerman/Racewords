ae5eb93dd0113e6b4de6e759783d004a23ef1caf
Zuzanna <zuzannacymerman1@gmail.com>
2020-12-01 19:18:56 +0100

    Movement

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,40 @@ namespace Racewords
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int speed = 8;
+            pictureBox1.Top = Movement.MoveSingleLine(pictureBox1.Top, speed);
+            pictureBox2.Top = Movement.MoveSingleLine(pictureBox2.Top, speed);
+            pictureBox3.Top = Movement.MoveSingleLine(pictureBox3.Top, speed);
+            pictureBox4.Top = Movement.MoveSingleLine(pictureBox4.Top, speed);
+            pictureBox5.Top = Movement.MoveSingleLine(pictureBox5.Top, speed);
+        }
+      
     }
 }
---
+++ Movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Racewords
+{
+    public class Movement
+    {
+       
+        public static int MoveSingleLine(int top, int speed)
+        {
+            if (top >= 1240)
+            {
+                top = 0;
+            }
+            else
+            {
+                top += speed;
+            }
+            return top;
+        }
+    }
+}
